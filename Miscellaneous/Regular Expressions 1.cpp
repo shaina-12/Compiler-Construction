ea8a0deb9e24141911ca1d0b3ec7cbd9837018b7
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2022-01-24 17:43:29 +0530

    Update Regular Expressions 1.cpp

--- Miscellaneous/Regular Expressions 1.cpp
+++ Miscellaneous/Regular Expressions 1.cpp
@@ -1,8 +1,17 @@
 # include <iostream>
 # include <regex>
+# include <string>
 using namespace std;
 int main(){
     regex r1("[0-1]*|0*1*");
-    cout<<regex_match("0111111000000",r1);
+    string s;
+    cout<<"Enter the string: ";
+    cin>>s;
+    if(regex_match(s,r1)){
+        cout<<"The string is accepted.";
+    }
+    else{
+        cout<<"The string is rejected.";
+    }
     return 0;
 }
