1e0e7753c6da71664760855d74c8ccd8b8816d53
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2022-01-24 17:58:48 +0530

    Update Regular Expressions 2.cpp

--- Miscellaneous/Regular Expressions 2.cpp
+++ Miscellaneous/Regular Expressions 2.cpp
@@ -1,1 +1,17 @@
-
+# include <iostream>
+# include <regex>
+# include <string>
+using namespace std;
+int main(){
+    regex r1("[(ab*c)(def)+(a*d+e)]+");
+    string s;
+    cout<<"Enter the string: ";
+    cin>>s;
+    if(regex_match(s,r1)){
+        cout<<"The string is accepted.";
+    }
+    else{
+        cout<<"The string is rejected.";
+    }
+    return 0;
+}
