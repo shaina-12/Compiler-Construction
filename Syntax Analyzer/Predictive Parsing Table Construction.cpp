9f65f67ca01b5be0e5080e71fa4d6d4dd577bf1d
Shaina Mehta <87758673+shaina-12@users.noreply.github.com>
2022-04-16 10:27:28 +0530

    Create Predictive Parsing Table Construction.cpp

---
+++ Syntax Analyzer/Predictive Parsing Table Construction.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+vector <string> prod= {"E->TR","R->+TR","R->#","T->FY","Y->*FY","Y->#","F->(E)","F->i"};
+vector <vector<char>> firsts = {{'(','i'},{'+','#'},{'(','i'},{'*','#'},{'(','i'}};
+vector <vector<char>> follow = {{'$',')'},{'$',')'},{'$',')','+'},{'$',')','+'},{'$',')','+','*'}};
+vector <char> term = {'i','+','*','(',')','$'};
+vector <char> nonterm = {'E','R','T','Y','F'};
+// '@' represents the error in the predictive parsing table
+int parsingTable[5][6] = {{INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX},
+                            {INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX},
+                            {INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX},
+                            {INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX},
+                            {INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX,INT_MAX}};
+vector<char> calFirst(string rhs){
+    vector <char> v;
+    //cout<<rhs.at(0)<<endl;
+    if(!isupper(rhs.at(0)) || rhs.at(0)=='#'){
+        //cout<<rhs.at(0)<<endl;
+        v.push_back(rhs.at(0));
+        return v;
+    }
+    char b = rhs.at(0);
+    auto i = find(nonterm.begin(),nonterm.end(),b);
+    int in = i - nonterm.begin();
+    vector <char> f = firsts[in];
+    if(find(f.begin(),f.end(),'#')==f.end()){
+        for(int j=0;j<f.size();j++){
+            if(find(v.begin(),v.end(),f[j])==v.end()){
+                v.push_back(f[j]);
+            }
+        }
+        return v;
+    }
+    else{
+        for(int i=1;i<rhs.size();i++){
+            char a = rhs.at(i);
+            auto it = find(nonterm.begin(),nonterm.end(),a);
+            int ind = it - nonterm.begin();
+            vector <char> fi = firsts[ind];
+            // stopping at not finding of '#
+            if(find(fi.begin(),fi.end(),'#')==fi.end()){
+                for(int j=0;j<fi.size();j++){
+                    // to avoid repeatation
+                    if(find(v.begin(),v.end(),fi[j])==v.end()){
+                        v.push_back(fi[j]);
+                    }
+                }
+                break;
+            }
+            else{
+                for(int j=0;j<fi.size();j++){
+                    // to avoid repeatation
+                    if(find(v.begin(),v.end(),fi[j])==v.end()){
+                        // to eliminate repetation of '#'
+                        if(i!=rhs.size()-1){
+                            if(fi[j]!='#'){
+                                v.push_back(fi[j]);
+                            }
+                        }
+                        else{
+                            v.push_back(fi[j]);  
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return v;
+}
+int main(){
+    for(int i=0;i<prod.size();i++){
+        int pno = i;
+        char lhs = prod[i].at(0);
+        //cout<<lhs<<" ";
+        string rhs = prod[i].substr(3);
+        //finding the index of lhs 
+        auto nt = find(nonterm.begin(),nonterm.end(),lhs);
+        int nind = nt - nonterm.begin();
+        // cal the first of rhs
+        vector<char> fir = calFirst(rhs);
+        /*
+        for(int k=0;k<fir.size();k++){
+            cout<<fir[k]<<" ";
+        }*/
+        // checking for '#' in the first of alpha
+        //cout<<fir.size()<<endl;
+        if(find(fir.begin(),fir.end(),'#')==fir.end()){
+            for(int j=0;j<fir.size();j++){
+                char a = fir.at(j);
+                //cout<<a<<endl;
+                auto t = find(term.begin(),term.end(),a);
+                int tind = t - term.begin();
+                parsingTable[nind][tind] = pno;
+            }
+        }
+        else{
+            // '#' is present in first of alpha
+            // cal the follow of lhs
+            vector<char> fol = follow[nind];
+            for(int j=0;j<fir.size();j++){
+                char a = fir.at(j);
+                if(a!='#'){
+                    auto t = find(term.begin(),term.end(),a);
+                    int tind = t - term.begin();
+                    parsingTable[nind][tind] = pno;
+                }
+            }
+            for(int j=0;j<fol.size();j++){
+                char a = fol.at(j);
+                auto t = find(term.begin(),term.end(),a);
+                int tind = t - term.begin();
+                parsingTable[nind][tind] = pno;
+            }
+        }
+    }
+    cout<<"The predictive parsing table is: "<<endl;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<6;j++){
+            if(parsingTable[i][j]==INT_MAX){
+                cout<<"@"<<" ";
+            }
+            else{
+                cout<<parsingTable[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
